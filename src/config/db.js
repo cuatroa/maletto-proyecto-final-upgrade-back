dcbee5148d70ed728d38a527c423181eb54b7c70
coritah44 <luranimal@gmail.com>
2020-11-07 11:14:39 +0100

    mongo atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/Maletto-2020';
+const url = process.env.MONGO_ATLAS_URL;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
 mongoose
-  .connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
+  .connect(url, connectionParams)
   .then(() => {
-    console.log(`Connected to DB: ${DB_URL}`);
+    console.log("Connected to database ");
   })
   .catch((err) => {
-    console.log(`Error connecting to DB: ${err.message}`);
+    console.error(`Error connecting to the database. \n${err}`);
   });
+
+//en caso de querer bbdd local:
+// const DB_URL =
+//   process.env.DB_URL || "mongodb://localhost:27017/fsft-sept-2020";
+// mongoose
+//   .connect(DB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log(`Connected to DB: ${DB_URL}`);
+//   })
+//   .catch((err) => {
+//     console.log(`Error connecting to DB: ${err.message}`);
+//   });
